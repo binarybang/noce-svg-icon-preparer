f972e2917dddb6adf923380cb4843e6872b68658
Ivan Kashtanov <iakashtanov@gmail.com>
2021-09-30 02:28:27 +0300

    build: fix writeVersion for pkg-constants

--- scripts/extract-pkg-info.js
+++ scripts/extract-pkg-info.js
@@ -1,18 +1,13 @@
-const fs = require('fs');
 const pkg = require('../package.json');
-
-function writePackageConstants(version) {
-  const pkgNameExport = `export const PACKAGE_NAME = '${pkg.name}';`;
-  const pkgVersionExport = `export const PACKAGE_VERSION = '${version}';`;
-
-  fs.writeFileSync('./src/utils/pkg-constants.ts', `${pkgNameExport}\n${pkgVersionExport}`);
-}
 
 module.exports.readVersion = function () {
   return '0.0.0';
 }
 
 module.exports.writeVersion = function (contents, version) {
-  writePackageConstants(version);
+  const pkgNameExport = `export const PACKAGE_NAME = '${pkg.name}';`;
+  const pkgVersionExport = `export const PACKAGE_VERSION = '${version}';`;
+
+  return `${pkgNameExport}\n${pkgVersionExport}`;
 }
 
